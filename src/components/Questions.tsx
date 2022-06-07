076a06052be5484da9d3e45b6f14c4c0828a304a
ryosei <liangqingshanye75@gmail.com>
2022-06-07 15:49:03 +0900

    ボタンが押されたときにポイント追加

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -5,14 +5,13 @@ function Questions(props: any) {
     return (
         <div>
             {props.ques.map((value: any, index: number) => {
-                { console.log(index) }
                 return (
                     <>
                         <div className="text-lg font-bold mt-[20px]">{value.content}</div><div className="pt-[20px] flex justify-evenly" key={index}>
                             <ul className="grid grid-cols-3 gap-x-5 m-10 max-w-md mx-auto">
-                                <AnswerButton index={value.index}>はい</AnswerButton>
-                                <AnswerButton index={value.index}>どちらでもない</AnswerButton>
-                                <AnswerButton index={value.index}>いいえ</AnswerButton>
+                                <AnswerButton index={index} pointCalculate={props.pointCalculate}>はい</AnswerButton>
+                                <AnswerButton index={index} pointCalculate={props.pointCalculate}>どちらでもない</AnswerButton>
+                                <AnswerButton index={index} pointCalculate={props.pointCalculate}>いいえ</AnswerButton>
                             </ul>
                         </div>
                         {(index === 5) && (
--- src/pages/question.tsx
+++ src/pages/question.tsx
@@ -8,15 +8,17 @@ interface Answer {
     content: string,
 }
 
-export default class App extends React.Component<{}, { questions: Answer[], currentId: string, dataset: any }> {
+export default class App extends React.Component<{}, { questions: Answer[], currentId: string, dataset: any ,pointNumber:number}> {
     constructor(props: any) {
         super(props);
         this.state = {
             questions: [],
             currentId: "init",
             dataset: defaultDataset,
+            pointNumber: 0,
         }
         this.selectAnswer = this.selectAnswer.bind(this)
+        this.pointCalculate = this.pointCalculate.bind(this)
     }
 
     displayNextQuestion = (nextQuestionId: string) => {
@@ -42,6 +44,16 @@ export default class App extends React.Component<{}, { questions: Answer[], curr
         }
     }
 
+    pointCalculate = (answerContent:string,) => {
+        let point = this.state.pointNumber;
+        if(answerContent === "はい") {
+            point += 1
+        } else if(answerContent === "いいえ"){
+            point -= 1
+        }
+        this.setState({pointNumber:point})
+    }
+
     componentDidMount() {
         const initAnswer = ""
         this.selectAnswer(initAnswer, this.state.currentId)
@@ -52,7 +64,7 @@ export default class App extends React.Component<{}, { questions: Answer[], curr
             <div>
                 <main className="relative h-[100vh] w-[100%]">
                     <div className="rounded-[4px] box-border h-[592px] max-w-[432px] py-0 px-[1rem] w-[100%] absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]">
-                        <Questions ques={this.state.questions} select={this.selectAnswer}/>
+                        <Questions ques={this.state.questions} select={this.selectAnswer} pointCalculate={this.pointCalculate}/>
                     </div>
                 </main>
             </div>
